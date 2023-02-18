1d8fdde235093a18b8ac6058f60168ee16be4dcb
niva45 <nivashamirtharajgmail.com>
2023-02-18 06:17:11 +0000

    2ndcommit

--- code.java
+++ code.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
 {  
 // character whose ASCII value to be found  
 char ch1 = 'a';  
-char ch2 = 'b';  
+char ch2 = 'b;  
 // variable that stores the integer value of the character  
 int asciivalue1 = ch1;  
 int asciivalue2 = ch2;  
